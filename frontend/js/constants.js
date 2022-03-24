2ce20085707da710853b2d1545527c40fdf3380e
Theaan van Staden <theaanvanstaden@gmail.com>
2022-03-24 20:22:58 +0700

    update colors

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x771a6392Ad165920A553A1056fFc956b279fE270"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Snail Pace NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Snail Pace is a collection of 1500 randomly generated snails living on the Ethereum Polygon blockchain. Snail Pace holders have the opportunity to contribute and fight against world hunger and more. A certain percentage will go to Action Against Hunger as a donation. Do not let your pace of life determine who you are...';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
