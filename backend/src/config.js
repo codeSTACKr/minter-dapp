3e054ac42eb47a727a4e667691b0a2776df6cd01
Theaan van Staden <theaanvanstaden@gmail.com>
2022-03-26 00:08:24 +0700

    updated start date

--- backend/src/config.js
+++ backend/src/config.js
@@ -62,7 +62,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-03-26T12:00:00+08:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-03-24T12:00:00+08:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-03-23T12:00:00+08:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 200; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x2d0381f88faA94Ffa6C3715daaA16CabF5e5059a"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
