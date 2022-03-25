c5aa3fbf63f4cfaa116b80f2fbf32657700add9d
Theaan van Staden <theaanvanstaden@gmail.com>
2022-03-26 00:00:23 +0700

    whitelist update

--- backend/src/config.js
+++ backend/src/config.js
@@ -67,7 +67,7 @@ const ROYALTY_SHARE = 200; // Percentage of the token price that goes to the roy
 const ROYALTY_ADDRESS = "0x2d0381f88faA94Ffa6C3715daaA16CabF5e5059a"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x2d0381f88faA94Ffa6C3715daaA16CabF5e5059a"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
