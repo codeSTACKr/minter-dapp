9bb89db9291a7a2bb46ebd228e299fa2bd8db830
Theaan van Staden <theaanvanstaden@gmail.com>
2022-03-26 00:15:18 +0700

    updated public mint start date

--- backend/src/config.js
+++ backend/src/config.js
@@ -59,7 +59,7 @@ const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MAT
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-26T12:00:00+08:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-03-23T12:00:00+08:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-03-23T12:00:00+08:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
