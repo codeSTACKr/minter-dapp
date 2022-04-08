f094adb3f9a2521efd9e586ef78528b61b443c40
Dannilo Franco <dannilofranco@gmail.com>
2022-04-08 17:51:24 -0300

    Putting code inside timeout block

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -550,6 +550,13 @@ async function loadInfo() {
   setTimeout(() => {
     const countdownCard = document.querySelector('.countdown');
     countdownCard.classList.add('show-card');
+
+
+    // Remove lines below for Mint GO Live
+    const countdownContainer = document.querySelector('.count-down');
+    countdownContainer.classList.add('hidden');
+    const presaleCoutnDown = document.getElementById("subHeading");
+    presaleCoutnDown.classList.add('hidden');
   }, 1000);
 
   let priceType = '';
@@ -603,11 +610,6 @@ async function loadInfo() {
   };
   mintButton.onclick = mint;
 
-  // Remove lines below for Mint GO Live
-  const countdownContainer = document.querySelector('.count-down');  
-  countdownContainer.classList.add('hidden');
-  const presaleCoutnDown = document.getElementById("subHeading");
-  presaleCoutnDown.classList.add('hidden');
 }
 
 function setTotalPrice() {
