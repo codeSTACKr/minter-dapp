4b3b79e0fee16af62076cabb2596183c2fc3f87c
TopBoyDev <7minuteswebsites@gmail.com>
2022-04-20 18:30:47 +0200

    updated

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
 const contractAddress = "0xA6e4Bdcde2d678c0124f646F2f5fB0418b00281E"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Nomad Tigers NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
