66e0942747d5a6f7fbb7888c132978f229bede39
shahrokh11 <sky2iran@gmail.com>
2022-04-22 14:02:34 +0430

    updated all

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x694ee3f85072908e23182f2878df183f21d51405"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
