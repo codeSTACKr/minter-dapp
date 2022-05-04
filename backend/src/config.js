8a7bf91f24ce2f7cb945c4cb17921f8e68dffa5b
tstringham <thomas@hottomali.com>
2022-05-04 16:54:16 -0700

    pubplish

--- backend/src/config.js
+++ backend/src/config.js
@@ -83,7 +83,7 @@ const OWNER_ADDRESS = '0x933623cb0Fa2586eF23479CDeE2Bea73628C2407';
 const TREASURY_ADDRESS = '0x933623cb0Fa2586eF23479CDeE2Bea73628C2407';
 const MAX_SUPPLY = 10000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 20; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-05-11T18:30:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
