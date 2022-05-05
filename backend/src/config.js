decebe3c08e7cff46bdd2ce393566623b7fa4772
tstringham <thomas@hottomali.com>
2022-05-04 23:50:48 -0700

    commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -94,7 +94,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x933623cb0Fa2586eF23479CDeE2Bea73628C2407"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ["0x933623cb0Fa2586eF23479CDeE2Bea73628C2407"]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
