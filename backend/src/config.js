b757ee7aa3bd3aa2c7327939fdc4630ddcb7a20b
ligitnft <101424767+ligitnft@users.noreply.github.com>
2022-05-14 08:47:16 -0700

    https://github.com/ligitnft/minter-dapp.git

--- backend/src/config.js
+++ backend/src/config.js
@@ -14,8 +14,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 3
-    000,
+    growEditionSizeTo: 3000,
     layersOrder: [
       { name: "Background" },
       { name: "Body" },
