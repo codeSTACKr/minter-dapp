a5fc17e9d2a600cbe993a046fb9e0ec38d8d2161
Ian <cryptoelmo22@gmail.com>
2022-05-19 10:01:49 +0800

    updating external url (possible?), welcome text, and setting JSON limit to 8k

--- backend/src/config.js
+++ backend/src/config.js
@@ -35,7 +35,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "nothingbutflex.nft", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://sites.google.com/view/cryptoelmo22", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Nothing But Flex NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Welcome to my first ever NFT project, complete from generating the art to deploying it on a dApp"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
