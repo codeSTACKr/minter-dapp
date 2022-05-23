70566752fd684ca82f3f3308bee48957b1749a4d
Just_Sean_Really <74496368+SeanDylan1982@users.noreply.github.com>
2022-05-23 07:04:00 +0200

    Rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,38 +7,39 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "GrouchyGorillas";
+const description = "The GrouchyGorillas have just landed on the and have made themselves a home right on the Polygon blockchain!";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 5000,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "1 Background" },
+      { name: "2 GrouchyGorilla" },
+      { name: "3 Tail" },
+      { name: "4 Hair" },
+      { name: "5 Eyes" },
+      { name: "6 Belt" },
+      { name: "7 Shoes" },
+      { name: "8 Accessories" },
     ],
   },
 ];
 
-const shuffleLayerConfigurations = true;
+const shuffleLayerConfigurations = false;
 
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 310,
+  height: 420,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://ipfs.io/ipfs/QmU62Vv2pFMjyXAUYDpkcUWNAQgpRYuqLRoDMhWLdpGD3c", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -49,25 +50,25 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'GrouchyGorillas';
+const CONTRACT_SYMBOL = 'GrG';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0xcc6CE1caF8D42ad3bf041f377f1F2C2FbC202b33';
+const TREASURY_ADDRESS = '0xcc6CE1caF8D42ad3bf041f377f1F2C2FbC202b33';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 100; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-06-01T00:00:00+02:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const PRESALE_MINT_START_DATE = "2022-05-23T00:00:00+02:00l"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const ROYALTY_SHARE = 0; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = "0xcc6CE1caF8D42ad3bf041f377f1F2C2FbC202b33"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0xcc6CE1caF8D42ad3bf041f377f1F2C2FbC202b33"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
@@ -75,8 +76,8 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_DESCRIPTION = "The GrouchyGorillas have just landed on the and have made themselves a home right on the Polygon blockchain!"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmSDSGLxiyZuYoHAjGMUYp73eiApZoAQpCdUGCLRvxdV17?filename=Blue.png, https://ipfs.io/ipfs/QmepNFkLdqpf1AZyb2Spq1VQ39BiAEGtT4vbnnTUSGvrJt?filename=Green.png, https://ipfs.io/ipfs/QmTYXSv2wZUstWtgGj54fDuxhDz9eBHHrUtKDh61RAGUgH?filename=Orange.png, https://ipfs.io/ipfs/QmUaWX3esakibKuJ4xP37Cv1A5i3tdPGJCD6CYcjkEb6BS?filename=Purple.png, https://ipfs.io/ipfs/QmeVHBgxUL5VbzdML21UifKU5WJ7pMvC9pSoRCvED9QvyG?filename=Red.png"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -93,13 +94,13 @@ try {
 // END NFTPort Info
 
 const solanaMetadata = {
-  symbol: "YC",
-  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
+  symbol: "GrG",
+  seller_fee_basis_points: 0, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://ipfs.io/ipfs/QmU62Vv2pFMjyXAUYDpkcUWNAQgpRYuqLRoDMhWLdpGD3c",
   creators: [
     {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
-      share: 100,
+      address: "0xcc6CE1caF8D42ad3bf041f377f1F2C2FbC202b33",
+      share: 0,
     },
   ],
 };
