4c678a4c3e00016d3cdb56365d1f1caebf45aba8
Ethan Irish <ethan.irish@live.com>
2022-06-01 22:02:37 -0400

    test

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -162,7 +162,7 @@ async function checkChain() {
 }
 
 async function loadInfo() {
-  window.info = await window.contract.methods.getInfo().call();
+ // window.info = await window.contract.methods.getInfo().call();
   const publicMintActive = await contract.methods.mintingActive().call();
   const presaleMintActive = await contract.methods.presaleActive().call();
   const mainHeading = document.getElementById("mainHeading");
