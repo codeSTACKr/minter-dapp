2558140de25094195bd28e49363c342998659899
Emanuele Correggia <emanuele.correggia86@gmail.com>
2022-06-18 18:27:21 +0200

    test

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0xd8C89A37FdC5eAA51EB92969AE7FD27B95D4d57e"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses 
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
