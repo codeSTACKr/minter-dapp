9eb844a8be3c5fa4e7c7db9eae56269af559b382
Duaa <temoka48@gmail.com>
2022-07-28 10:53:00 +0300

    update project description

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,18 +7,18 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "CRAZY YOUNGSTERS";
+const description = "STAY LOUD AND WEIRD";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
-const backgroundLayer = { name: "Common Layers/BACKGROUND" }; //NO ---
-const backHairLayer = { name: "Common Layers/backhair" }; //YES - YES - NO - CRASHES WHEN UNCOMMENTED - YES - NO
-const HairLayer = { name: "Common Layers/HAIR" }; //NO
-const eyesLayer = { name: "Common Layers/EYES" }; //NO
-const mouthLayer = { name: "Common Layers/MOUTH" };//NO
-const eyebrowsLayer = { name: "Common Layers/EYEBROWS" };//NO
-const faceDecorationLayer = { name: "Common Layers/DECORATION" }; //YES YES NO NO NO
-const clothesLayer = { name: "Common Layers/CLOTHES" }; //NO ---
+const backgroundLayer = { name: "Common Layers/BACKGROUND" }; 
+const backHairLayer = { name: "Common Layers/backhair" }; 
+const HairLayer = { name: "Common Layers/HAIR" }; 
+const eyesLayer = { name: "Common Layers/EYES" }; 
+const mouthLayer = { name: "Common Layers/MOUTH" };
+const eyebrowsLayer = { name: "Common Layers/EYEBROWS" };
+const faceDecorationLayer = { name: "Common Layers/DECORATION" }; 
+const clothesLayer = { name: "Common Layers/CLOTHES" };
 
 const LayerOrder = (i) => {
   // console.log(i);
@@ -99,22 +99,22 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby, polygon, or ethereum
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'CRAZY YOUNGSTERS';
+const CONTRACT_SYMBOL = 'CYS';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0x1D2814c36b4b2835413890d91202644C49C9F120';
+const TREASURY_ADDRESS = '0x1D2814c36b4b2835413890d91202644C49C9F120';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.07; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-10-29T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const PRESALE_MINT_START_DATE = "2022-10-28T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const ROYALTY_SHARE = 700; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = "0x1D2814c36b4b2835413890d91202644C49C9F120"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
