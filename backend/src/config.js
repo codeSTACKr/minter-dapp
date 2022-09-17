45d1cc2df68a33977de5e930982d3829719262c7
ouss2811 <ferawouss2811@gmail.com>
2022-09-17 03:02:42 +0200

    yeah

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,7 +61,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-10-10T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-09-17T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-09-16T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x6AdaF03FaB41eDa20169dBA94c24984D29be22f6"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
