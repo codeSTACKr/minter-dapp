ccd1d7c97fdbd8c24fb625abb9e274258d372cee
Edo Prasnikar <edo.prasnikar@gmail.com>
2023-01-08 22:10:10 +0100

    tet 2

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,15 +1,15 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x6a74a3E6A707d2CB0eA09ab0f24ed1B2f50F5814"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to Edo 1!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
+const h1_presale_coming_soon = "NFT Drop Coming Soon Edo!!";
+const h1_presale_mint = "Pre-Sale Minting Edo Open!!";
+const h1_public_mint = "Public Minting Open Edo!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
+const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates HARD POVKZ!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
