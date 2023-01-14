f07165055d241f28948e1f94415006846373da49
Edo Prasnikar <edo.prasnikar@gmail.com>
2023-01-14 15:00:47 +0100

    14. jan 04

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,15 +1,15 @@
 const contractAddress = "0x6a74a3E6A707d2CB0eA09ab0f24ed1B2f50F5814"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "Welcome to Edo 1!!";
+const welcome_h1 = "Welcome to Dx0xE Projcet";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'TEST EDO decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon Edo!!";
-const h1_presale_mint = "Pre-Sale Minting Edo Open!!";
-const h1_public_mint = "Public Minting Open Edo!!";
+const welcome_p = 'Get ready, to be a part of Dx0xE NFT project. Together we will launch unique NFT Collection. Connect MetaMask NOW and Mint your future!"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const h1_presale_mint = "Pre-Sale Minting Open!!";
+const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates HARD POVKZ!";
+const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
