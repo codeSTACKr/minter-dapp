0859e89ef90570112fb862edd155c7cb70896096
Edo Prasnikar <edo.prasnikar@gmail.com>
2023-01-21 14:15:41 +0100

    21.jan

--- backend/src/config.js
+++ backend/src/config.js
@@ -37,7 +37,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "https://edo.com", // Replace with your website or remove this line if you do not have one.
+  external_url: "http://eternalblocksntf.com/", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -48,13 +48,13 @@ const LIMIT = 10; // Your API key rate limit
 const CHAIN = 'goerli'; // only goerli, polygon, or ethereum
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'EDO Collection';
-const CONTRACT_SYMBOL = 'EP';
+const CONTRACT_NAME = 'Eternal Blocks by Dx0xE';
+const CONTRACT_SYMBOL = 'EB';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = '0xc2a2b32b71fE1adC51eAAaf6DD8fbc39fbed296A';
-const TREASURY_ADDRESS = '0xc2a2b32b71fE1adC51eAAaf6DD8fbc39fbed296A';
-const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.01; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const OWNER_ADDRESS = '0xe038AD3F4850196f9A903338276CEC6ad5FC3BeC';
+const TREASURY_ADDRESS = '0xe038AD3F4850196f9A903338276CEC6ad5FC3BeC';
+const MAX_SUPPLY = 777; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MINT_PRICE = 0.03; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED! 
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
@@ -62,14 +62,14 @@ const PUBLIC_MINT_START_DATE = "2023-01-10T11:30:48+00:00"; // This is required.
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2023-01-07T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 7000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xc2a2b32b71fE1adC51eAAaf6DD8fbc39fbed296A"; // Address that will receive the royalty
+const ROYALTY_SHARE = 750; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = "0xe038AD3F4850196f9A903338276CEC6ad5FC3BeC"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ['0xc2a2b32b71fE1adC51eAAaf6DD8fbc39fbed296A','0xE1f4A97Bb0D1E4CdDb7BFBBcB0eC57b4b94f805d']; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ['0xe038AD3F4850196f9A903338276CEC6ad5FC3BeC','0xE1f4A97Bb0D1E4CdDb7BFBBcB0eC57b4b94f805d']; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x6a74a3E6A707d2CB0eA09ab0f24ed1B2f50F5814s"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
@@ -94,7 +94,7 @@ try {
 const solanaMetadata = {
   symbol: "YC",
   seller_fee_basis_points: 0, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
+  external_url: "http://eternalblocksntf.com/",
   creators: [
     {
       address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
